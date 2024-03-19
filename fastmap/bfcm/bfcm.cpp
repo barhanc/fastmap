8f8f5951fa357cd8dd777730857127b1d2f6a296
Bartosz Hanc <bartosz.hanc02@gmail.com>
2024-03-19 02:24:56 +0100

    update

--- fastmap/bfcm/bfcm.cpp
+++ fastmap/bfcm/bfcm.cpp
@@ -1,6 +1,6 @@
+#include <omp.h>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
-#include <stdbool.h>
 
 #include <algorithm>
 #include <iostream>
@@ -11,32 +11,35 @@
 
 namespace py = pybind11;
 
-double bfcm(py::array_t<double, py::array::f_style> D, int n_v, int n_c) {
-    auto r = D.unchecked<4>();
+double bfcm(py::array_t<double, py::array::f_style> D, int nv, int nc) {
+    auto data = D.unchecked<4>();
 
-    int sigma[n_c] = {0};
-    for (int i = 0; i < n_c; i++) sigma[i] = i;
+    int sigma[nc] = {0};
+    for (int i = 0; i < nc; i++) sigma[i] = i;
 
     double best_res = std::numeric_limits<double>::infinity();
+    double cost[nv * nv] = {0};
+    int64_t a[nv] = {0}, b[nv] = {0};
 
     do {
-        double cost[n_v * n_v] = {0};
-        for (int i = 0; i < n_v; i++)
-            for (int j = 0; j < n_v; j++)
-                for (int k = 0; k < n_c; k++)
-                    cost[i * n_v + j] += r(i, j, k, sigma[k]);
+        for (int i = 0; i < nv; i++)
+            for (int j = 0; j < nv; j++) {
+                double acc = 0;
+                for (int k = 0; k < nc; k++)
+                    acc += data(i, j, k, sigma[k]);
+                cost[i * nv + j] = acc;
+            }
 
-        int64_t a[n_v], b[n_v];
-        int ret = solve_rectangular_linear_sum_assignment(n_v, n_v, cost, false, a, b);
+        int ret = solve_rectangular_linear_sum_assignment(nv, nv, cost, false, a, b);
         if (ret == RECTANGULAR_LSAP_INFEASIBLE || ret == RECTANGULAR_LSAP_INVALID)
             throw std::runtime_error("LSAP problem is infeasible or invalid");
 
         double res = 0;
-        for (int i = 0; i < n_v; i++)
-            res += cost[a[i] * n_v + b[i]];
+        for (int i = 0; i < nv; i++)
+            res += cost[a[i] * nv + b[i]];
 
         best_res = std::min(best_res, res);
-    } while (std::next_permutation(sigma, sigma + n_c));
+    } while (std::next_permutation(sigma, sigma + nc));
 
     return best_res;
 }

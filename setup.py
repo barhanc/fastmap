43720050661f8b8f54f203217ea041f16f8a2fc9
Bartosz Hanc <bartosz.hanc02@gmail.com>
2024-03-20 23:48:11 +0100

    update

--- setup.py
+++ setup.py
@@ -10,8 +10,6 @@ setup(
     license="MIT",
     description="Optimized implementations of algorithms computing distance measures for Maps of Elections.",
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Natural Language :: English",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: Implementation :: CPython",

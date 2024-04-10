945313563ae51fd918ff80b1a3208f41b7d56d03
Bartosz Hanc <bartosz.hanc02@gmail.com>
2024-04-10 16:00:00 +0200

    Change tests

--- tests/test.py
+++ tests/test.py
@@ -1,38 +1,83 @@
-import numpy as np
 import time
-import fastmap.bfcm
+import numpy as np
+
+import fastmap.example.wrapper as mdist
 import fastmap.isomorphic
 
 from mapel.elections.distances import cppdistances as dist
 
+
+def hamm_bf(U: np.ndarray[bool | int], V: np.ndarray[bool | int]) -> int:
+    from itertools import permutations
+
+    assert U.shape == V.shape
+    assert len(U.shape) == 2
+
+    nv, nc = U.shape
+    candidates = np.arange(nc)
+    U, V = U.astype(bool), V.astype(bool)
+
+    best_res = float("inf")
+    for nu in permutations(range(nv)):
+        for sigma in permutations(range(nc)):
+            sigma = np.array(sigma)
+            res = 0
+            for i in range(nv):
+                u = set(sigma[candidates[U[i]]])
+                v = set(candidates[V[nu[i]]])
+                res += len(u.symmetric_difference(v))
+            best_res = min(best_res, res)
+
+    return best_res
+
+
 if __name__ == "__main__":
-    nv, nc = 5, 8
+    nv, nc = 10, 10
     print(f"Prob. size : nv={nv}, nc={nc}")
 
-    X = np.array([np.random.permutation(nc) for _ in range(nv)])
-    Y = np.array([np.random.permutation(nc) for _ in range(nv)])
+    print("Spearman")
+    U = np.array([np.random.permutation(nc) for _ in range(nv)])
+    V = np.array([np.random.permutation(nc) for _ in range(nv)])
 
     s = time.perf_counter()
-    res = dist.speard(X, Y)
+    stdres = dist.speard(U, V)
     e = time.perf_counter()
     t1 = e - s
-    print(f"Target(C++): res={res}, t={e-s:.4f}s")
+    print(f"Target(C++): res={stdres}, t={e-s:.4f}s")
 
+    print()
     s = time.perf_counter()
-    pos_X, pos_Y = X.argsort(), Y.argsort()
-    if nv < nc:
-        pos_X, pos_Y = pos_X.T, pos_Y.T
-
-    res = fastmap.bfcm.bfcm(pos_X.astype(np.int32), pos_Y.astype(np.int32))
+    best = float("inf")
+    for _ in range(10):
+        res = mdist.spearman(U, V, method="cd")
+        best = min(best, res)
+        print(f"Custom C   : res={res:4d}, t={e-s:.4f}s")
     e = time.perf_counter()
     t2 = e - s
-    print(f"Custom C   : res={res}, t={e-s:.4f}s")
+    print(f"Custom C Best   : {best}")
+    print()
 
-    s = time.perf_counter()
-    D = np.abs(np.subtract.outer(X.argsort(), Y.argsort())).swapaxes(1, 2)
-    res = fastmap.isomorphic.bfcm(X, Y, D)
-    e = time.perf_counter()
-    t3 = e - s
-    print(f"Numpy      : res={res}, t={e-s:.4f}s")
+    # s = time.perf_counter()
+    # D = np.abs(np.subtract.outer(U.argsort(), V.argsort())).swapaxes(1, 2)
+    # res = fastmap.isomorphic.bfcm(U, V, D)
+    # e = time.perf_counter()
+    # t3 = e - s
+    # print(f"Numpy      : res={res}, t={e-s:.4f}s")
 
-    print(f"{t1 / t2:4.2f}x")
+    print(f"Speed up: {t1 / t2:4.2f}x")
+    print(f"Approx ratio: {best / stdres:.2f}")
+
+    # ==============================================================================================
+
+    nv, nc = 4, 3
+    print(f"Prob. size : nv={nv}, nc={nc}")
+    print("Hamming")
+
+    U = np.array([np.random.randint(0, 2, size=nc) for _ in range(nv)])
+    V = np.array([np.random.randint(0, 2, size=nc) for _ in range(nv)])
+
+    res = mdist.hamming(U, V)
+    print(res)
+
+    res = hamm_bf(U, V)
+    print(res)

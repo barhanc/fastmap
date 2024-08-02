049d90090720f304c3e930fb7519935487e4e417
Bartosz Hanc <bartosz.hanc02@gmail.com>
2024-08-02 20:20:31 +0200

    Add description of the package to the __init__.py file

--- fastmap/__init__.py
+++ fastmap/__init__.py
@@ -1,6 +1,8 @@
-"""_summary_
-
-TODO:...
+"""
+Fastmap is a small Python library that provides optimized C implementations (as well as convenient
+Python wrappers) of algorithms computing structural similarity between elections used in the Map of
+Elections framework, such as the isomorphic swap and Spearman distances, Hamming distance, pairwise
+distance as well as distances based on diversity, agreement and polarization of the elections.
 """
 
 from fastmap._wrapper import spearman

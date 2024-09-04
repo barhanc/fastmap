05881a2bf83d08f3c00e4bdfe4280d547acdbb09
Bartosz Hanc <bartosz.hanc02@gmail.com>
2024-09-05 00:27:57 +0200

    Update testpair.py

--- tests/testpair.py
+++ tests/testpair.py
@@ -35,35 +35,45 @@ def bf(M_U: np.ndarray, M_V: np.ndarray):
     return best
 
 
-from scipy.optimize import quadratic_assignment
+nv, nc = 100, 9
+culture1 = ORDINAL_CULTURES[random.randint(0, len(ORDINAL_CULTURES) - 1)]
+culture2 = ORDINAL_CULTURES[random.randint(0, len(ORDINAL_CULTURES) - 1)]
 
-if __name__ == "__main__":
-    print("PAIRWISE\n")
+U = mapel.generate_ordinal_election(
+    culture_id=culture1["id"],
+    num_candidates=nc,
+    num_voters=nv,
+    **culture1["params"],
+    seed=0,
+)
+V = mapel.generate_ordinal_election(
+    culture_id=culture2["id"],
+    num_candidates=nc,
+    num_voters=nv,
+    **culture2["params"],
+    seed=0,
+)
 
-    nv, nc = 100, 10
-    culture1 = ORDINAL_CULTURES[random.randint(0, len(ORDINAL_CULTURES) - 1)]
-    culture2 = ORDINAL_CULTURES[random.randint(0, len(ORDINAL_CULTURES) - 1)]
+print("PAIRWISE\n")
+print(f"Candidates {nc} :: Votes {nv} :: Culture1 {culture1['id']} :: Culture2 {culture2['id']}\n")
 
-    print(f"Candidates {nc} :: Votes {nv} :: Culture1 {culture1['id']} :: Culture2 {culture2['id']}\n")
+t1 = time.monotonic()
+d1 = bf(U.votes_to_pairwise_matrix(), V.votes_to_pairwise_matrix())
+t1 = time.monotonic() - t1
+print(f"Python :: {d1:6.3f} :: Time {t1:6.3f}")
 
-    U = mapel.generate_ordinal_election(
-        culture_id=culture1["id"], num_candidates=nc, num_voters=nv, **culture1["params"]
-    )
-    V = mapel.generate_ordinal_election(
-        culture_id=culture2["id"], num_candidates=nc, num_voters=nv, **culture2["params"]
-    )
+for trial in range(5):
 
-    t1 = time.monotonic()
-    d1 = bf(U.votes_to_pairwise_matrix(), V.votes_to_pairwise_matrix())
-    t1 = time.monotonic() - t1
-    print(f"Python :: {d1:6.3f} :: Time {t1:6.3f}")
+    print(f"\nTrial : {trial}\n")
 
     t2 = time.monotonic()
-    d2 = fastmap.pairwise(U.votes_to_pairwise_matrix(), V.votes_to_pairwise_matrix())
+    d2 = fastmap.pairwise(
+        U.votes_to_pairwise_matrix(), V.votes_to_pairwise_matrix(), method="faq", repeats=300, tol=1e-8, seed=0
+    )
     t2 = time.monotonic() - t2
-    print(f"C(faq) :: {d2:6.3f} :: Time {t2:6.3f}s :: Approx. ratio {d2 / d1:.3f}")
+    print(f"C(faq) :: {d2:6.3f} :: Time {t2:6.3f}s :: Approx. ratio {d2 / d1 if d1 > 0 else d1 == d2:.3f}")
 
     t3 = time.monotonic()
-    d3 = fastmap.pairwise(U.votes_to_pairwise_matrix(), V.votes_to_pairwise_matrix(), method="aa", repeats=300)
+    d3 = fastmap.pairwise(U.votes_to_pairwise_matrix(), V.votes_to_pairwise_matrix(), method="aa", repeats=300, seed=0)
     t3 = time.monotonic() - t3
-    print(f"C(aa)  :: {d3:6.3f} :: Time {t3:6.3f}s :: Approx. ratio {d3 / d1:.3f}")
+    print(f"C(aa)  :: {d3:6.3f} :: Time {t3:6.3f}s :: Approx. ratio {d3 / d1 if d1 > 0 else d1 == d3:.3f}")

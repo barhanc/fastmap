d1422a58aa59e1a3c4641aa828be96cb37f5bde2
Bartosz Hanc <bartosz.hanc02@gmail.com>
2024-09-04 16:46:38 +0200

    Add docs to stack.h

--- fastmap/utils/stack.h
+++ fastmap/utils/stack.h
@@ -1,3 +1,21 @@
+/**
+ * @file queue.h
+ * @brief Simple implementation of a stack or Last-In-First-Out (LIFO) queue.
+ *
+ * This file contains a basic implementation of a generic stack using a linked list. The stack
+ * allows for elements to be added to and removed from the top.
+ *
+ * NOTE: This implementation is not thread-safe.
+ *
+ * Public fields:
+ * - `size`: number of elements on the stack
+ *
+ * Public functions:
+ * - `Queue *stack_alloc()`: allocate memory for the stack and return pointer to it
+ * - `void push(Stack *s, void *item)`: push a generic item on the top of the stack
+ * - `void *pop(Stack *s)`: remove and return the generic item from the top of the stack
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,7 +28,7 @@ typedef struct StackNode
 typedef struct Stack
 {
     size_t size;
-    StackNode *head;
+    StackNode *top;
 } Stack;
 
 Stack *
@@ -18,7 +36,7 @@ stack_alloc ()
 {
     Stack *stack = malloc (sizeof (Stack));
     stack->size = 0;
-    stack->head = NULL;
+    stack->top = NULL;
     return stack;
 }
 
@@ -27,8 +45,8 @@ push (Stack *stack, void *item)
 {
     StackNode *node = malloc (sizeof (StackNode));
     node->item = item;
-    node->next = stack->head;
-    stack->head = node;
+    node->next = stack->top;
+    stack->top = node;
     stack->size++;
     return;
 }
@@ -42,10 +60,11 @@ pop (Stack *stack)
         return NULL;
     }
 
-    void *item = stack->head->item;
-    StackNode *tmp = stack->head;
-    stack->head = stack->head->next;
+    void *item = stack->top->item;
+    StackNode *tmp = stack->top;
+    stack->top = stack->top->next;
     stack->size--;
     free (tmp);
+
     return item;
 }

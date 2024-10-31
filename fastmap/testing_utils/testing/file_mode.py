a02814ef94b9eac3dbb368237530bb2ff5d68f02
Bartosz Hanc <bartosz.hanc02@gmail.com>
2024-10-31 18:07:49 +0100

    Revert "kkemeny done with some testing, deployment wip"
    This reverts commit dec6e66052c77a49b28809b0907c94686a6f3e14.

--- fastmap/testing_utils/testing/file_mode.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class FileMode(str, Enum):
-    READ = 'r'
-    WRITE = 'w'
-    APPEND = 'a'
--- setup.py
+++ setup.py
@@ -10,23 +10,15 @@ CFLAGS = ["-Ofast"]
 if cpu_has["AVX2"]:
     CFLAGS.append("-mavx2")
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="fastmap",
     version=__version__,
-    packages=find_packages(include=["fastmap", "testing_utils"]),
+    packages=find_packages(),
     license="MIT",
     description="Optimized implementations of algorithms computing distance measures for Maps of Elections.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     classifiers=[
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering",
     ],
     install_requires=[
         "numpy",
@@ -63,73 +55,6 @@ setup(
             extra_compile_args=CFLAGS,
             include_dirs=["fastmap/lap/", "fastmap/qap/", "fastmap/utils/"],
         ),
-        Extension(
-            name="fastmap._agreement_index",
-            sources=["fastmap/agreement_index.c"],
-            extra_compile_args=CFLAGS,
-            include_dirs=["fastmap/kkemeny/"],
-        ),
-        Extension(
-            name="fastmap._polarization_index",
-            sources=["fastmap/polarization_index.c"],
-            extra_compile_args=CFLAGS,
-            include_dirs=["fastmap/kkemeny/"],
-        ),
-        Extension(
-            name="fastmap._diversity_index",
-            sources=["fastmap/diversity_index.c"],
-            extra_compile_args=CFLAGS,
-            include_dirs=["fastmap/kkemeny/"],
-        ),
-        Extension(
-            name="fastmap._kemeny_ranking",
-            sources=["fastmap/kemeny_ranking.c"],
-            extra_compile_args=CFLAGS,
-            include_dirs=["fastmap/kkemeny/"]
-        ),
-        Extension(
-            name="fastmap._local_search_kkemeny_single_k",
-            sources=["fastmap/local_search_kkemeny_single_k.c"],
-            extra_compile_args=CFLAGS,
-            include_dirs=["fastmap/kkemeny/"]
-        ),
-        Extension(
-            name="fastmap._local_search_kkemeny",
-            sources=["fastmap/local_search_kkemeny.c"],
-            extra_compile_args=CFLAGS,
-            include_dirs=["fastmap/kkemeny/"]
-        ),
-        Extension(
-            name="fastmap._polarization_1by2Kemenys",
-            sources=["fastmap/polarization_1by2Kemenys.c"],
-            extra_compile_args=CFLAGS,
-            include_dirs=["fastmap/kkemeny/"]
-        ),
-        Extension(
-            name="fastmap._greedy_kmeans_summed",
-            sources=["fastmap/greedy_kmeans_summed.c"],
-            extra_compile_args=CFLAGS,
-            include_dirs=["fastmap/kkemeny/"]
-        ),
-        Extension(
-            name="fastmap._greedy_kKemenys_summed",
-            sources=["fastmap/greedy_kkemenys_summed.c"],
-            extra_compile_args=CFLAGS,
-            include_dirs=["fastmap/kkemeny/"]
-        ),
-        Extension(
-            name="fastmap._greedy_2kKemenys_summed",
-            sources=["fastmap/greedy_2kKemenys_summed.c"],
-            extra_compile_args=CFLAGS,
-            include_dirs=["fastmap/kkemeny/"]
-        ),
-        Extension(
-            name="fastmap._greedy_kKemenys_divk_summed",
-            sources=["fastmap/greedy_kKemenys_divk_summed.c"],
-            extra_compile_args=CFLAGS,
-            include_dirs=["fastmap/kkemeny/"]
-        ),
     ],
     include_dirs=[np.get_include()],
-    options={"build_ext": {"verbose": True}}
 )
